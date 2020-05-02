817c0f0e8fb6f140f875ab23f2b1eaa2442fab74
Victor Rodrigues <victorc.rodrigues@gmail.com>
2020-05-02 18:41:36 +0200

    Add dialyzer

--- mix.exs
+++ mix.exs
@@ -30,7 +30,8 @@ defmodule Contex.MixProject do
     [
       {:nimble_strftime, "~> 0.1.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:sweet_xml, "~> 0.6.6", only: :test}
+      {:sweet_xml, "~> 0.6.6", only: :test},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
