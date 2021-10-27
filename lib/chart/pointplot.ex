d4d1571ab3506f22180e721f075590aa6f89dd6c
Shad <shadowRR@users.noreply.github.com>
2021-10-27 07:28:35 +0200

    Adding plot options to PointPlot and LinePlot (#57)
    Adding support for the plot options, allowing PointPlot and LinePlot to show/hide the x/y axis.
    
    I've added default plot options as requested. Note the default_plot_options are a copy of the original from the plot.ex file, except it's now a map instead of a keyword list (if i understand correctly, that's the goal and it's converted by Plot.parse_attributes/1 before the being sent to the underlying implementation).

--- lib/chart/pointplot.ex
+++ lib/chart/pointplot.ex
@@ -52,6 +52,12 @@ defmodule Contex.PointPlot do
     colour_palette: :default
   ]
 
+  @default_plot_options %{
+    show_x_axis: true,
+    show_y_axis: true,
+    legend_setting: :legend_none
+  }
+
   @type t() :: %__MODULE__{}
 
   @doc ~S"""
@@ -304,17 +310,31 @@ defmodule Contex.PointPlot do
   def get_svg_legend(_), do: ""
 
   @doc false
-  def to_svg(%PointPlot{} = plot) do
+  def to_svg(%PointPlot{} = plot, plot_options) do
     plot = prepare_scales(plot)
     x_scale = plot.x_scale
     y_scale = plot.y_scale
 
-    axis_x = get_x_axis(x_scale, plot)
-    axis_y = Axis.new_left_axis(y_scale) |> Axis.set_offset(get_option(plot, :width))
+    plot_options = Map.merge(@default_plot_options, plot_options)
+
+    x_axis_svg =
+      if plot_options.show_x_axis,
+        do:
+          get_x_axis(x_scale, plot)
+          |> Axis.to_svg(),
+        else: ""
+
+    y_axis_svg =
+      if plot_options.show_y_axis,
+        do:
+          Axis.new_left_axis(y_scale)
+          |> Axis.set_offset(get_option(plot, :width))
+          |> Axis.to_svg(),
+        else: ""
 
     [
-      Axis.to_svg(axis_x),
-      Axis.to_svg(axis_y),
+      x_axis_svg,
+      y_axis_svg,
       "<g>",
       get_svg_points(plot),
       "</g>"
