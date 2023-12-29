e5859a059b5245e301c626a764115a4316c76161
mindok <mindok@users.noreply.github.com>
2023-12-29 17:10:08 +1100

    First pass OHLC chart

---
+++ lib/chart/gallery/ohlc_charts.ex
@@ -0,0 +1,32 @@
+defmodule Contex.Gallery.OHLCCharts do
+  import Contex.Gallery.Sample, only: [graph: 1]
+
+  @moduledoc """
+  A gallery of OHLC Charts.
+
+  > #### Have one to share? {: .warning}
+  >
+  > Do you have an interesting plot you want to
+  > share? Something you learned the hard way that
+  > should be here, or that's just great to see?
+  > Just open a ticket on GitHub and we'll post it here.
+
+
+  """
+
+  @doc """
+  Some OHLC charts.
+
+
+  #{graph(title: "A simple candle OHLC chart",
+  file: "ohlc_candle.sample"
+  )}
+
+  #{graph(title: "A simple tick OHLC chart",
+  file: "ohlc_tick.sample"
+  )}
+
+
+  """
+  def plain(), do: 0
+end
